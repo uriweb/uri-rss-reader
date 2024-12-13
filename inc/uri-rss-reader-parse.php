f6c1169580d03de53ba514fce606156db9ef8945
alexandragauss <alexandra_gauss@uri.edu>
2024-12-13 12:56:32 -0500

    Update shortcode and parsing functions

--- inc/uri-rss-reader-parse.php
+++ inc/uri-rss-reader-parse.php
@@ -5,39 +5,125 @@
  *
  * @package uri-rss-reader
  */
-function uri_rss_reader_load_xml($feed_data)
-{
-    // Load the XML from the URL
-    $xml = simplexml_load_file($feed_data);
-    return $xml;
+
+ /**
+  * Get the XML data from the URL
+  */
+function uri_rss_reader_get_xml ($url) {
+    $response = wp_safe_remote_get($url);
+    $body = (wp_remote_retrieve_body($response));
+    $rss = simplexml_load_string($body);
+    return $rss;
 }
 
-function uri_rss_reader_feed_list($xml, $exclude_urls)
+/**
+ * Load the XML data from the URL of the feed
+ * @param str feed url
+ * @param arr urls to exclude from feed
+ * @return arr feed data
+ */
+function uri_rss_reader_get_array($rss, $exclude_urls)
 {
-    foreach ($xml->channel->item as $item) {
-        // Check if the <link> element matches an excluded URL
+
+    // Initialize an array to hold the feed data
+    $array_feed = [];
+
+    // Loop through each RSS item
+    foreach ($rss->channel->item as $item) {
+        // Check if the <link> element matches the excluded URL
         if (!in_array((string)$item->link, $exclude_urls)) {
-            // Display the item
-            echo '<div class="uri-rss-reader-item">';
-            // Check if the <thumbnail> element exists
-            $thumbnail = $item->children('http://search.yahoo.com/mrss/')->thumbnail;
+            $feed_item = [
+                'title' => (string)$item->title,
+                'link'  => (string)$item->link,
+            ];
 
-            if ($thumbnail && isset($thumbnail->attributes()['url'])) {
-                $thumbAttr = $thumbnail->attributes();
-                echo '<div class="uri-rss-reader-thumbnail">';
-                echo '<img src="' . $thumbAttr['url'] . '">';
-                //end div class="uri-rss-reader-thumbnail
-                echo '</div>';
+            // Check if media:thumbnail exists and extract the URL and alt attribute
+            if (isset($item->children('media', true)->thumbnail)) {
+                $thumbnail = $item->children('http://search.yahoo.com/mrss/')->thumbnail->attributes();
+                $feed_item['media:thumbnail'] = [
+                    'url' => (string)$thumbnail['url'],
+                    'alt' => (string)$thumbnail['alt'] ?: 'Featured image for ' . $item->title
+                ];
             }
-            echo '<div class="uri-rss-reader-title">';
-            echo '<h3><a href="' . $item->link . '">' . $item->title . "</a></h3>";
-            echo '</div>';
-            echo '<div class="uri-rss-reader-excerpt">';
-            echo "<p>" . $item->description . "</p>";
-            echo '</div>';
-            //end uri-rss-reader-item
-            echo '</div>'; 
-
+            // Add the item to the array feed
+            $array_feed[] = $feed_item;
         }
     }
+    return $array_feed;
+}
+
+
+
+/*
+        if ( isset( $response->channel ) && !empty( $response->channel ) && '200' == wp_remote_retrieve_response_code( $response ) ) {
+              // hooray, all is well!
+              $body = ( wp_remote_retrieve_body ( $response ) );
+              $xml_string = simplexml_load_string($body);
+              $json_obj = json_encode($xml_string);
+              return json_decode($json_obj, TRUE );
+          } else {
+      
+              // still here?  Then we have an error condition
+              //this doesn't work correctly
+      
+              if ( is_wp_error ( $response ) ) {
+                  $error_message = $response->get_error_message();
+                  echo 'There was an error with the RSS Reader Plugin: ' . $error_message;
+                  return FALSE;
+              }
+              if ( '200' != wp_remote_retrieve_response_code( $response ) ) {
+                  echo $response;
+                  return FALSE;
+              }
+      
+              // still here?  the error condition is indeed unexpected
+              echo "Empty response from server?";
+              return FALSE;
+          }
+      */
+
+/**
+ * Render the feed
+ * @param arr feed data
+ * @param arr attributes from shortcode
+ * @return arr feed data
+ */
+function uri_rss_reader_display($feed_data, $attributes)
+{
+    ob_start();
+    print $attributes['before'];
+    //loop through array
+    ?>
+    
+    <div class="uri-rss-reader-feed">
+    <h3 class="latest-news">Latest News</h3>
+        <?php
+
+    foreach ($feed_data as $element) {
+?>
+        <div class="uri-rss-reader-item">
+            <?php
+            if (isset($element['media:thumbnail'])) {
+            ?>
+                <div class="uri-rss-reader-image">
+                    <img src="<?php print $element['media:thumbnail']['url'] ?>" alt="<?php print $element['media:thumbnail']['alt'] ?>">
+                </div>
+            <?php
+            } else {
+                ?>
+<div class="no-thumbnail"></div>
+                <?php
+            }
+            ?>
+            <div class="uri-rss-reader-title-link">
+                <a href=" <?php print $element['link'] ?> "><?php print $element['title'] ?></a>
+            </div>
+        </div>
+<?php
+    }
+    ?> </div>
+    <?php
+    print $attributes['after'];
+    $feed_output = ob_get_clean();
+    return $feed_output;
 }
--- inc/uri-rss-reader-shortcode.php
+++ inc/uri-rss-reader-shortcode.php
@@ -20,11 +20,33 @@ function uri_rss_reader_shortcode($attributes, $shortcode)
         'after' => '</div>'
     ), $attributes, $shortcode);
 
-    $feed_data = uri_rss_reader_load_xml( $attributes['url']);
     $exclude_urls = explode(", ", $attributes['exclude']);
+    $feed_data = uri_rss_reader_load_xml( $attributes['url']);
+    
+    
     $output = uri_rss_reader_feed_list($feed_data, $exclude_urls);
+    
 
     return $output;
 }
 
 add_shortcode('uri-rss-reader', 'uri_rss_reader_shortcode');
+
+function uri_rss_reader_2_shortcode($attributes, $shortcode) {
+    // default attributes
+    $attributes = shortcode_atts(array(
+        'url' => NULL,
+        'exclude' => NULL,
+        'before' => '<div class="uri-rss-reader">',
+        'after' => '</div>'
+    ), $attributes, $shortcode);
+
+    $exclude_urls = explode(", ", $attributes['exclude']);
+$xml_data = uri_rss_reader_get_xml($attributes['url']);
+$feed_data = uri_rss_reader_get_array($xml_data, $exclude_urls);
+$output = uri_rss_reader_display ($feed_data, $attributes);
+return $output;
+
+}
+
+add_shortcode('uri-rss-reader2', 'uri_rss_reader_2_shortcode');
