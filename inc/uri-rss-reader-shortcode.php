ad56be25e216521a7503f5aa767e9f69d81634dd
alexandragauss <alexandra_gauss@uri.edu>
2024-12-26 09:07:48 -0500

    Add caching

--- inc/uri-rss-reader-shortcode.php
+++ inc/uri-rss-reader-shortcode.php
@@ -9,44 +9,39 @@
 /**
  * Create a shortcode for displaying rss news feeds
  */
+
 function uri_rss_reader_shortcode($attributes, $shortcode)
 {
-
     // default attributes
     $attributes = shortcode_atts(array(
         'url' => NULL,
+        'display' => '20',
         'exclude' => NULL,
+        'cache' => '1 hour',
         'before' => '<div class="uri-rss-reader">',
         'after' => '</div>'
     ), $attributes, $shortcode);
 
+    // Turn excluded urls into an array
     $exclude_urls = explode(", ", $attributes['exclude']);
-    $feed_data = uri_rss_reader_load_xml( $attributes['url']);
-    
-    
-    $output = uri_rss_reader_feed_list($feed_data, $exclude_urls);
-    
+
+    // save cache time to live as site option
+    update_site_option('uri_rss_reader_recency', $attributes['cache']);
+
+    //Get raw xml data
+   // $xml_data = uri_rss_reader_get_xml($attributes['url']);
+
+    //Get array from raw xml data
+    //$feed_data = uri_rss_reader_build_array($xml_data, $attributes['cache']);
+
+    //go straight to cache check instead 
+    $feed_data = uri_rss_reader_get_the_feed($attributes['url']);
+
+    //Display the feed
+    $output = uri_rss_reader_display($feed_data, $attributes, $exclude_urls);
+
 
     return $output;
 }
 
 add_shortcode('uri-rss-reader', 'uri_rss_reader_shortcode');
-
-function uri_rss_reader_2_shortcode($attributes, $shortcode) {
-    // default attributes
-    $attributes = shortcode_atts(array(
-        'url' => NULL,
-        'exclude' => NULL,
-        'before' => '<div class="uri-rss-reader">',
-        'after' => '</div>'
-    ), $attributes, $shortcode);
-
-    $exclude_urls = explode(", ", $attributes['exclude']);
-$xml_data = uri_rss_reader_get_xml($attributes['url']);
-$feed_data = uri_rss_reader_get_array($xml_data, $exclude_urls);
-$output = uri_rss_reader_display ($feed_data, $attributes);
-return $output;
-
-}
-
-add_shortcode('uri-rss-reader2', 'uri_rss_reader_2_shortcode');
